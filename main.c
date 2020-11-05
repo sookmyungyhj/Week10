3992e07a6becc009d93ed5c30566eaeee0734519
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-11-05 22:24:31 +0900

    실습5-1
    배열값을 키보드 입력으로 받도록 프로그램 수정

--- main.c
+++ main.c
@@ -3,16 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap (int *x, int *y) {
-int temp;
-temp = *x;
-*x = *y;
-*y = temp;
-} 
+int main(void){
+int i;
+int grade[5];
 
-void main(void) {
-int a = 3;
-int b = 5;
-swap(&a, &b);
-printf("a:%i, b:%i\n", a, b);
+for (i=0;i<5;i++){
+printf("input value (%i)= ", i);
+scanf("%d", &grade[i]);
+}
+
+for (i=0;i<5;i++)
+printf("grade[%d] = %d\n", i, grade[i]);
+
+return 0;
 }

1d868d76f9711cb642890cb587666981d2eadcc8
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-11-05 22:04:58 +0900

    실습3-2

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(void) {
-int i = 4; //300으로 변경해서도 해보기
+int i = 300; //300으로 변경해서도 해보기
 int *pi = &i;
 char *pc = &i;
 

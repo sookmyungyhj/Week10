87550b27edf2ec2ef519c3eb3b6cad57c1702e5f
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-11-05 22:01:48 +0900

    실습2

--- main.c
+++ main.c
@@ -3,15 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void)
-{
+int main(void) {
+	
 int i = 10;
-char c = 69;
-float f = 12.3;
+char c = 'a';
 
-printf("i : %i %p \n", &i, &i);
-printf("c: %i %p \n", &c, &c);
-printf("f : %i %p \n", &f, &f);
+int *iptr; //정수형 포인터 변수 iptr 선언;
+char *cptr; //문자형 포인터 변수 cptr 선언;
+int *iptr2; //정수형 포인터 변수 iptr2 선언; 
+
+iptr = &i; //iptr이 i를 가리키게 함;
+cptr = &c; //cptr이 c를 가리키게 함;
+iptr2 = iptr; //iptr2가 iptr이 가리키는 곳을 동일하게 가리키게 함;
+
+printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
+printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
+printf("iptr2 : %p, %i\n", iptr2, *iptr2);
 
 return 0;
 }

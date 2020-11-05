73c0327796c993edfb43a7fb88b4c26541539572
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-11-05 21:53:14 +0900

    실습PPT VER

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int i = 10;
 char c = 69;
 float f = 12.3;
 
-printf("i : %i %p \n", i, &i);
-printf("c: %i %p \n", c, &c);
-printf("f : %i %p \n", f, &f);
+printf("i : %i %p \n", &i, &i);
+printf("c: %i %p \n", &c, &c);
+printf("f : %i %p \n", &f, &f);
 
 return 0;
 }
